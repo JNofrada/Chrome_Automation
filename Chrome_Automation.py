1a9eccc7ab441b7bd29f393d8e23fa3dd59cc135
Jeremiah <jnnofrada@gmail.com>
2019-06-03 14:16:19 -0400

    auto done

--- Chrome_Automation.py
+++ Chrome_Automation.py
@@ -1,1 +1,14 @@
 
+import chrome_bookmarks
+import webbrowser
+import sys
+
+def main():
+    for url in chrome_bookmarks.urls:
+        print(url.url)
+        if (url.url == 'https://valiantny.sharepoint.com/sites/CMChangeManagement' or url.url == 'https://developers.google.com/api-client-library/python/'):
+            continue
+        webbrowser.open_new_tab(url.url)
+    sys.exit("0")
+
+main()
